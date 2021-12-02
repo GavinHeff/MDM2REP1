56731203e05f8504a85ef7f80869f2cd538b763c
GavinHeff <gavlaheff02@gmail.com>
2021-12-02 17:58:22 +0000

    Fixed modellingNoArgparse.py by replacing everything Mike wrote with stuff from Google

--- modellingNoArgparse.py
+++ modellingNoArgparse.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 30 17:29:51 2021
+Created on Thu Dec  2 16:35:07 2021
 
 @author: gavla
 """
-import numpy as np
+
 from scipy.integrate import odeint
-import math
+import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 length = 9
 width = 3
@@ -24,41 +25,42 @@ rudderHeight=0.5
 density=1000
 boatMass=3000
 
+l = length
+w = width
+h = height
+v = boatSpeed
+beta = rudderAngle
+s = rudderLength
+A = rudderLength*rudderHeight
+rho = density
+m = boatMass
 
-force = [(density*rudderHeight*rudderLength*(boatSpeed)**2)*math.sin(rudderAngle)*(-math.cos(rudderAngle)),(density*rudderHeight*rudderLength*((boatSpeed)**2)*math.sin(rudderAngle)*(math.sin(rudderAngle))),0]
-F = np.array(force)
-radius = [-((length)/2+(rudderLength)*math.cos(rudderAngle)/2),-(rudderLength)*math.sin(rudderAngle)/2,0]
-R = np.array(radius)
-# torque = [0,0,args.density*args.rudderHeight*args.rudderLength*((args.boatSpeed)^2)*((math.sin(args.rudderAngle))^2)*args.rudderLength*(math.cos(args.rudderAngle)+0.5)]
-# T = np.array(torque)
-T = np.cross(F,R)
-I = (1/3)*(((length)**2)+((length)**2))*boatMass
-a = [0,0,T[2]/I]
+def Derivs(x,t):
+    # boatAngVel=x[0]
+    # alpha = x[1]
+    
+    ode = x[0] - (3*rho*A*v**2*(math.sin(beta)**2)*(math.sin(beta)+l/2))/(m*(l**2+w**2))
+    return(ode)    
+    
+    # force = [(density*rudderHeight*rudderLength*(boatSpeed)**2)*math.sin(rudderAngle)*(-math.cos(rudderAngle)),(density*rudderHeight*rudderLength*((boatSpeed)**2)*math.sin(rudderAngle)*(math.sin(rudderAngle))),0]
+    # F = np.array(force)
+    # radius = [-((length)/2+(rudderLength)*math.cos(rudderAngle)/2),-(rudderLength)*math.sin(rudderAngle)/2,0]
+    # R = np.array(radius)
+    # T = np.cross(F,R)#
+    # I = (1/3)*(((length)**2)+((length)**2))*boatMass
+    # a = [0,0,T[2]/I]   
+    
+    # d2Alphadt2 = a[2]
 
-print(F,R,T,I,a)
-
-def Derivs(t, x):
-    Alpha = x[0]
-    d2Alphadt2 = a
-    return[d2Alphadt2]
-
-Alpha0 = 0
-
-x0 = [Alpha0]
-print(Derivs(x=x0, t=0))
-
-tr = np.linspace(0,10,1000)
-solution = odeint(Derivs,x0,tr)
-
-Alpha = solution[:,0]
-
-plt.plot(tr,Alpha,'b',label='Boat Heading')
-plt.legend(loc='best')
-plt.title('Change in boat heading against time')
-plt.xlabel('Time, secs')
-plt.ylabel('Boat heading')
-plt.grid(True)
+x0 = [0]
+ts = np.linspace(1,10,1000)
+xs = odeint(Derivs, x0, ts)
+ys = xs[:,0]
+plt.plot(ts,xs,'-')
+plt.plot(ts,ys,'r*')
+plt.xlabel('Time/s')
+plt.ylabel('Heading/degrees')
+plt.title('Heading change with time')
 plt.show
 
-dangveldt = (3*density*rudderHeight*rudderLength*(boatSpeed**2)*((math.sin(rudderAngle))**2)*rudderLength*math.cos(rudderAngle)+length/2)/((length**2 + width**2)*boatMass)
-print(dangveldt/2)
+
