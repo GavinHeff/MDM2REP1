486e614cb0fc1a9e9abd47cbf919e5884da0a3b7
Ian Vazquez Perez <34981855+ianvazperez@users.noreply.github.com>
2021-12-03 19:07:59 +0000

    Make variables as inputs with constraints and fix bugs

--- modellingNoArgparse.py
+++ modellingNoArgparse.py
@@ -1,44 +1,71 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec  2 16:35:07 2021
-
-@author: gavla
-"""
-
 from scipy.integrate import odeint
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-length = 9
-width = 3
-height = 3
-windSpeed = 0
-windHeading = 0
-boatSpeed=10
-boatHeading=0
-waterSpeed=0
-waterHeading=0
-rudderAngle=-20
-rudderLength=0.5
-rudderHeight=0.5
-density=1000
-boatMass=3000
+l = length = 9.144 # 30 feet in metres
+rho = density = 1000 # density of water
 
-l = length
-w = width
-h = height
-v = boatSpeed
-beta = rudderAngle
-s = rudderLength
-A = rudderLength*rudderHeight
-rho = density
-m = boatMass
+
+while True:
+    w = width = int(input("What is the width of the boat in metres: "))
+    if w < 0:
+        print("The width can not be negative.")
+        w = width = int(input("What is the width of the boat in metres: "))
+    else:
+        break
+    break
+
+while True:
+    v = boatspeed = int(input("What is the speed of the boat: "))
+    if v < 0:
+        print("The wind speed can not be negative.")
+        v = boatspeed = int(input("What is the speed of the boat: "))
+    else:
+        break
+    break
+
+while True:
+    beta = rudderAngle = int(input("What is the angle of the rudder in degrees: "))
+    if abs(beta) > 90:
+        print("The rudder angle can't be bigger than 90 degrees")
+        beta = rudderAngle = int(input("What is the angle of the rudder in degrees: "))
+    else:
+        break
+    break
+
+while True:
+    s = rudderLength = int(input("What is the length of the rudder: "))
+    if s < 0:
+        print("The rudder length can not be negative.")
+        s = rudderLength = int(input("What is the length of the rudder: "))
+    else:
+        break
+    break
+
+while True:
+    rudderHeight = int(input("What is the height of the rudder: "))
+    if rudderHeight < 0:
+        print("The rudder height can not be negative.")
+        rudderHeight = int(input("What is the height of the rudder: "))
+    else:
+        break
+    break
+
+while True:
+    m = boatMass = int(input("What is the weight of the boat: "))
+    if m < 0:
+        print("The weight can not be negative.")
+        m = boatMass = int(input("What is the weight of the boat: "))
+    else:
+        break
+    break
+
+pi = math.pi 
+A = Area = rudderLength*rudderHeight # Area of rudder
+Izz = 1/3 * m * (l^2 + w^2) # Second moment of area for a cuboid
 
 def Derivs(x,t):
-    # boatAngVel=x[0]
-    # alpha = x[1]
-    
     ode = x[0] - (3*rho*A*v**2*(math.sin(beta)**2)*(math.sin(beta)+l/2))/(m*(l**2+w**2))
     return(ode)    
     
@@ -51,6 +78,8 @@ def Derivs(x,t):
     # a = [0,0,T[2]/I]   
     
     # d2Alphadt2 = a[2]
+
+    
 
 x0 = [0]
 ts = np.linspace(1,10,1000)
