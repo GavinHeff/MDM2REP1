9ded6c92e4a04c83203e7b7704364c9eee67d9fb
Ian Vazquez Perez <34981855+ianvazperez@users.noreply.github.com>
2021-12-04 14:58:01 +0000

    Fixed second moment of area equation and changed variables to floats

--- modellingNoArgparse.py
+++ modellingNoArgparse.py
@@ -8,62 +8,62 @@ rho = density = 1000 # density of water
 
 
 while True:
-    w = width = int(input("What is the width of the boat in metres: "))
+    w = width = float(input("What is the width of the boat in metres: "))
     if w < 0:
         print("The width can not be negative.")
-        w = width = int(input("What is the width of the boat in metres: "))
+        w = width = float(input("What is the width of the boat in metres: "))
     else:
         break
     break
 
 while True:
-    v = boatspeed = int(input("What is the speed of the boat: "))
+    v = boatspeed = float(input("What is the speed of the boat: "))
     if v < 0:
         print("The wind speed can not be negative.")
-        v = boatspeed = int(input("What is the speed of the boat: "))
+        v = boatspeed = float(input("What is the speed of the boat: "))
     else:
         break
     break
 
 while True:
-    beta = rudderAngle = int(input("What is the angle of the rudder in degrees: "))
+    beta = rudderAngle = float(input("What is the angle of the rudder in degrees: "))
     if abs(beta) > 90:
         print("The rudder angle can't be bigger than 90 degrees")
-        beta = rudderAngle = int(input("What is the angle of the rudder in degrees: "))
+        beta = rudderAngle = float(input("What is the angle of the rudder in degrees: "))
     else:
         break
     break
 
 while True:
-    s = rudderLength = int(input("What is the length of the rudder: "))
+    s = rudderLength = float(input("What is the length of the rudder: "))
     if s < 0:
         print("The rudder length can not be negative.")
-        s = rudderLength = int(input("What is the length of the rudder: "))
+        s = rudderLength = float(input("What is the length of the rudder: "))
     else:
         break
     break
 
 while True:
-    rudderHeight = int(input("What is the height of the rudder: "))
+    rudderHeight = float(input("What is the height of the rudder: "))
     if rudderHeight < 0:
         print("The rudder height can not be negative.")
-        rudderHeight = int(input("What is the height of the rudder: "))
+        rudderHeight = float(input("What is the height of the rudder: "))
     else:
         break
     break
 
 while True:
-    m = boatMass = int(input("What is the weight of the boat: "))
+    m = boatMass = float(input("What is the weight of the boat: "))
     if m < 0:
         print("The weight can not be negative.")
-        m = boatMass = int(input("What is the weight of the boat: "))
+        m = boatMass = float(input("What is the weight of the boat: "))
     else:
         break
     break
 
 pi = math.pi 
 A = Area = rudderLength*rudderHeight # Area of rudder
-Izz = 1/3 * m * (l^2 + w^2) # Second moment of area for a cuboid
+Izz = 1/3 * m * (l**2 + w**2) # Second moment of area for a cuboid
 
 def Derivs(x,t):
     ode = x[0] - (3*rho*A*v**2*(math.sin(beta)**2)*(math.sin(beta)+l/2))/(m*(l**2+w**2))
